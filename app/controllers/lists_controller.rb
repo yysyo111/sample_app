0e93a99368c0da57f082a387e9697ea7bfbce28f
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-05 14:44:48 +0000

    [Finish]3-8【バリデーションの設定】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,12 @@ class ListsController < ApplicationController
   end
   
   def create
-     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)  
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
   end
 
   def index
@@ -50,4 +50,3 @@ end
 # privateより後に定義されたメソッドは、アクションとして認識されなくなり、URLと対応できなくなり,メソッドの呼び出しに制限をかけることができる
 # privateメソッドの名前は、「モデル名_params」
 
-# Listからallメソッドを呼び出しているので、listsテーブルに保存されている全てのデータを取得すること
