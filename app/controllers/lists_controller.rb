0fe58a0f7ff8c9171cb318e74aee53ecedb06ccc
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-05 02:22:24 +0000

    [Finish]3-2【投稿機能を作ろう】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,15 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new #list.newとすることでlistモデルのtitle,bodyを格納できる
+  end
+  
+  def create
+     # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -10,4 +20,17 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private #「ここから下はこのcontrollerの中でしか呼び出せません」
+  # ストロングパラメータ(「マスアサインメント脆弱性」というセキュリティ上の問題を防ぐことができる)
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
 end
+
+
+# params :formから送られてくるデータはparamsの中に入っています。
+# require :送られてきたデータの中からモデル名(ここでは:list)を指定し、データを絞り込みます。
+# permit :requireで絞り込んだデータの中から、保存を許可するカラムを指定します。
+# privateより後に定義されたメソッドは、アクションとして認識されなくなり、URLと対応できなくなり,メソッドの呼び出しに制限をかけることができる
+# privateメソッドの名前は、「モデル名_params」
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  post 'lists' => 'lists#create' #List.newといった新しい情報がmodelオプションに設定されている場合は、CreateアクションのURLへ送信といった風に、データの送信先を自動的に判別することができます
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
