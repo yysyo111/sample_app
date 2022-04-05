4837cb47392db2c921121c097f63300e0515992d
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-05 12:01:47 +0000

    [Finish]3-5【編集機能を作ろう】

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create' #List.newといった新しい情報がmodelオプションに設定されている場合は、CreateアクションのURLへ送信といった風に、データの送信先を自動的に判別することができます
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
-  get '/top' => "homes#top"
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  get 'top' => 'homes#top'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
